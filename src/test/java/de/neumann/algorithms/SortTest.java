99e89c74cfecb11c98702c91e9ae519673455ae4
AlphaDango <alpha.dango98@gmail.com>
2022-04-03 14:08:51 +0200

    Made most sorting algorithms generic

--- src/test/java/de/neumann/algorithms/SortTest.java
+++ src/test/java/de/neumann/algorithms/SortTest.java
@@ -11,12 +11,12 @@ public class SortTest {
     @Test
     public void bubbleSort() {
         int length = 10 + rd.nextInt(100);
-        int[] array = new int[length];
+        Integer[] array = new Integer[length];
 
         for(int i = 0; i < length; i++)
             array[i] = 1 + rd.nextInt(1000);
 
-        int[] sortedArray = array.clone();
+        Integer[] sortedArray = array.clone();
 
         Arrays.sort(sortedArray);
         System.out.println("BubbleSort\n_______________\n");
@@ -28,12 +28,12 @@ public class SortTest {
     @Test
     public void insertionSort() {
         int length = 10 + rd.nextInt(100);
-        int[] array = new int[length];
+        Integer[] array = new Integer[length];
 
         for(int i = 0; i < length; i++)
             array[i] = 1 + rd.nextInt(1000);
 
-        int[] sortedArray = array.clone();
+        Integer[] sortedArray = array.clone();
 
         Arrays.sort(sortedArray);
         System.out.println("InsertionSort\n_______________\n");
@@ -59,20 +59,36 @@ public class SortTest {
         System.out.println("Sorted Array: " + Arrays.toString(array)+"\n");
     }
 
-    @Test
+   @Test
     public void selectionSort() {
         int length = 10 + rd.nextInt(100);
-        int[] array = new int[length];
+        Integer[] array = new Integer[length];
 
         for(int i = 0; i < length; i++)
             array[i] = 1 + rd.nextInt(1000);
 
-        int[] sortedArray = array.clone();
+        Integer[] sortedArray = array.clone();
 
         Arrays.sort(sortedArray);
         System.out.println("SelectionSort\n_______________\n");
         System.out.println("Unsorted Array: " + Arrays.toString(array));
         assertArrayEquals(sortedArray,Sort.selectionSort(array));
+        System.out.println("Sorted Array: " + Arrays.toString(array)+"\n");
+    }
+
+    @Test
+    public void bogoSort() {
+        Integer[] array = new Integer[10];
+
+        for(int i = 0; i < 10; i++)
+            array[i] = 1 + rd.nextInt(1000);
+
+        Integer[] sortedArray = array.clone();
+
+        Arrays.sort(sortedArray);
+        System.out.println("SelectionSort\n_______________\n");
+        System.out.println("Unsorted Array: " + Arrays.toString(array));
+        assertArrayEquals(sortedArray,Sort.bogoSort(array));
         System.out.println("Sorted Array: " + Arrays.toString(array)+"\n");
     }
 }
